93227cb0026bf789701752b5515a90f9b6de3d5b
jjdcabasolo <abasolojohnjourish@gmail.com>
2019-10-23 07:22:36 +0000

    [SEO] Added canonical links to react-helmet

--- src/components/navigation/Helmet.js
+++ src/components/navigation/Helmet.js
@@ -11,6 +11,7 @@ const Helmet = ({ isDefault }) => {
         <title>Movies</title>
         <meta name="description" content="Another The Movie Database Rip-off's (ATMDbRo) list of movies that offers 5 types of categories - Now Playing, Upcoming, Popular, Top Rated, and Highest Grossing." />
         <meta name="keywords" content="ATMDbRo,atmdbro,Another The Movie Database Rip-off,another the movie database rip-off,ATMDbRo Movies,atmdbro movies" />
+        <link rel="canonical" href="https://jjdcabasolo.github.io/another-moviedb-rip-off/#/movies" />
       </ReactHelmet>
     );
   } else if (activeTab === 'tvshows' && !isDefault) {
@@ -19,6 +20,7 @@ const Helmet = ({ isDefault }) => {
         <title>TV Shows</title>
         <meta name="description" content="Another The Movie Database Rip-off's (ATMDbRo) list of tv shows that offers 4 types of categories - Airing Today, On The Air, Popular, and Top Rated." />
         <meta name="keywords" content="ATMDbRo,atmdbro,Another The Movie Database Rip-off,another the movie database rip-off,ATMDbRo TV Shows,atmdbro tv shows" />
+        <link rel="canonical" href="https://jjdcabasolo.github.io/another-moviedb-rip-off/#/tvshows" />
       </ReactHelmet>
     );
   }
@@ -26,6 +28,7 @@ const Helmet = ({ isDefault }) => {
     <ReactHelmet titleTemplate="%s - Another TMDb Rip-off" defaultTitle="Another TMDb Rip-off">
       <meta name="description" content="Another The Movie Database Rip-off (ATMDbRo) lists movies and TV shows of different categories - all coming from TMDb. It is TMDb, but it is a rip-off." />
       <meta name="keywords" content="ATMDbRo,atmdbro,Another The Movie Database Rip-off,another the movie database rip-off" />
+      <link rel="canonical" href="https://jjdcabasolo.github.io/another-moviedb-rip-off/" />
     </ReactHelmet>
   );
 };
