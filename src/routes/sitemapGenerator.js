d413598d753c23c41e4a5efb4073ed02adc1f61c
jjdcabasolo <abasolojohnjourish@gmail.com>
2019-10-25 01:04:34 +0000

    [SEO] Added sitemap

---
+++ src/routes/sitemapGenerator.js
@@ -0,0 +1,16 @@
+require('@babel/register')({
+	extends: './.babelrc',
+})
+
+import Sitemap from 'react-router-sitemap';
+import routes from './index';
+
+function generateSitemap() {
+  return (
+    new Sitemap(routes)
+      .build("http://jjdcabasolo.github.io/another-moviedb-rip-off")
+      .save("./public/sitemap.xml")
+  );
+}
+
+generateSitemap();
