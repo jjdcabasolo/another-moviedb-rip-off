d0b66772812256636ef395d980995540723a7a52
jjdcabasolo <abasolojohnjourish@gmail.com>
2019-11-18 08:20:30 +0000

    [Movie] Replaced Cast UI with proper UI for depicting humans (Avatar)

--- src/components/movie/MovieDetails/CastCard.js
+++ src/components/movie/MovieDetails/CastCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import {
@@ -7,7 +7,9 @@ import {
   CardActionArea,
   CardMedia,
   Typography,
+  Avatar,
 } from '@material-ui/core';
+import { BrokenImage } from '@material-ui/icons';
 
 import { MOVIE_DRAWER_TMDB_IMAGE_PREFIX } from '../../../constants';
 
@@ -43,26 +45,42 @@ const useStyles = makeStyles(theme => ({
     letterSpacing: '0.03em',
     fontWeight: theme.typography.fontWeightMedium,
   },
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
+  text: {
+    marginTop: theme.spacing(1),
+  },
 }));
 
 const CastCard = ({ content, col }) => {
   const classes = useStyles();
 
+  const gridContainerRef = useRef(null);
+  const avatarRef = useRef(null);
+
+  useEffect(() => {
+    console.log(gridContainerRef)
+    // avatarRef.current.style.width = gridContainerRef.current.offsetWidth;
+    // console.log(gridContainerRef, avatarRef)
+  }, [gridContainerRef.current]);
+
   if (!content) return <></>;
 
-  const renderBrokenImage = () => (
-    <div className={classes.brokenImgContainer}>
-      <Typography variant="body1">Cast has no available image.</Typography>
-    </div>
-  );
+  // const renderBrokenImage = () => (
+  //   <div className={classes.brokenImgContainer}>
+  //     <Typography variant="body1">Cast has no available image.</Typography>
+  //   </div>
+  // );
 
-  let imagePath = MOVIE_DRAWER_TMDB_IMAGE_PREFIX;
-  if (content && content.profile_path) imagePath += `/w780${content.profile_path}`;
-  else imagePath = renderBrokenImage();
+  // let imagePath = MOVIE_DRAWER_TMDB_IMAGE_PREFIX;
+  // if (content && content.profile_path) imagePath += `/w780${content.profile_path}`;
+  // else imagePath = renderBrokenImage();
 
   return (
-    <Grid item xs={col}>
-      <Card>
+    <Grid item xs={col} container justify="center" alignItems="center" direction="column">
+      {/* <Card>
         <CardActionArea>
           { !(typeof (imagePath) === 'string') && imagePath }
           <CardMedia className={classes.mediaCard} image={imagePath} />
@@ -75,7 +93,25 @@ const CastCard = ({ content, col }) => {
             </Typography>
           </div>
         </CardActionArea>
-      </Card>
+      </Card> */}
+      <Grid item>
+        <Avatar
+          src={content.profile_path !== null ? `${MOVIE_DRAWER_TMDB_IMAGE_PREFIX}/w780${content.profile_path}` : ''}
+          className={classes.avatar}
+        >
+          {content.profile_path === null && <BrokenImage fontSize="large" />}
+        </Avatar>
+      </Grid>
+      <Grid item className={classes.text}>      
+        <Typography variant="body1" className={classes.title} align="center" noWrap>
+          {content.character}
+        </Typography>
+      </Grid>
+      <Grid item>      
+        <Typography vairant="body2" className={classes.subtitle} align="center" color="textSecondary" noWrap>
+          {content.name}
+        </Typography>
+      </Grid>
     </Grid>
   );
 };
