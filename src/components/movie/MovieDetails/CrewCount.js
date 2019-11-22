263cce20da8d9b881958beecd7a7b37d48aea7f8
jjdcabasolo <abasolojohnjourish@gmail.com>
2019-11-22 01:02:46 +0000

    [Movie] Improved crew count UI

--- src/components/movie/MovieDetails/CrewCount.js
+++ src/components/movie/MovieDetails/CrewCount.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid, Tooltip } from '@material-ui/core';
+import { Divider, Grid, Tooltip, Typography } from '@material-ui/core';
 import { HelpTwoTone } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
@@ -9,31 +9,41 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(1),
     fontSize: theme.typography.overline.fontSize,
   },
+  divider: {
+    height: theme.spacing(4),
+  },
 }));
 
-const CrewCount = ({ count, col, label, isTotal }) => {
+const CrewCount = ({ count, col, label, isTotal, divider }) => {
   const classes = useStyles();
 
   return (
-    <Grid item xs={col} container justify="center" alignItems="center" direction="column">
-      <Grid item>
-        <Typography variant="h4">{count}</Typography>
+    <>
+      <Grid item xs={col - 1} container justify="center" alignItems="center" direction="column">
+        <Grid item>
+          <Typography variant="h4">{count}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="overline">
+            {label}
+            {isTotal && (
+              <Tooltip
+                enterTouchDelay={0}
+                leaveTouchDelay={2500}
+                title="Crew count is wholly based on the efforts of the TMDb community. It may or may not reflect the exact head count."
+              >
+                <HelpTwoTone className={classes.icon} />
+              </Tooltip>
+            )}
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="overline">
-          { label }
-          { isTotal && (
-            <Tooltip
-              enterTouchDelay={0}
-              leaveTouchDelay={2500}
-              title="Crew count is wholly based on the efforts of the TMDb community. It may or may not reflect the exact head count."
-            >
-              <HelpTwoTone className={classes.icon} />
-            </Tooltip>
-          )}
-        </Typography>
-      </Grid>
-    </Grid>
+      {divider && (
+        <Grid item>
+          <Divider orientation="vertical" className={classes.divider} />
+        </Grid>
+      )}
+    </>
   );
 };
 
--- src/constants/movies.js
+++ src/constants/movies.js
@@ -43,3 +43,66 @@ export const MOVIE_DRAWER_CATEGORY_CHIPS = [
     isActive: e => e === 'highestGrossing',
   },
 ];
+
+// CREW PLAN
+// // default
+// department: "Directing",
+//   job: "Director",
+// department: "Writing",
+// department: "Production",
+//   job: "Producer",
+//   job: "Co-Producer",
+//   job: "Executive Producer",
+//   job: "Casting",
+// // see more
+// department: "Sound",
+//   job: "Original Music Composer",
+// department: "Camera",
+//   job: "Director of Photography",
+// department: "Editing",
+//   job: "Editor",
+// department: "Art",
+//   job: "Production Design",
+//   job: "Art Direction",
+// department: "Costume & Make-Up",
+//   job: "Costume Design",
+//   job: "Makeup Artist",
+// department: "Crew", // count
+// department: "Lighting", // count
+// department: "Visual Effects", // count
+// // total count of crew at the end
+
+export const CREW_TO_DISPLAY = [
+  {
+    identifier: 'director',
+    label: () => 'Film Direction',
+  },
+  {
+    identifier: 'writer',
+    label: a => `Screenwriter${a > 1 ? 's' : ''}`,
+  },
+  {
+    identifier: 'production',
+    label: () => 'Key Production',
+  },
+  {
+    identifier: 'composer',
+    label: () => 'Music',
+  },
+  {
+    identifier: 'cinematography',
+    label: () => 'Cinematography',
+  },
+  {
+    identifier: 'editor',
+    label: a => `Film Editor${a > 1 ? 's' : ''}`,
+  },
+  {
+    identifier: 'costume',
+    label: () => 'Art',
+  },
+  {
+    identifier: 'makeup',
+    label: () => 'Hair and Make-up',
+  },
+];
