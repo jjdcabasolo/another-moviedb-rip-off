1e3d0863f658489e9475354665907700e286bc35
jjdcabasolo <abasolojohnjourish@gmail.com>
2020-01-23 02:05:22 +0000

    [Refactor] Tooltip and dark mode components

---
+++ src/components/common/DarkModeToggle.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  IconButton,
+  ListItemText,
+  ListItem,
+  ListItemIcon,
+} from '@material-ui/core';
+import {
+  Brightness2TwoTone,
+  BrightnessLowTwoTone,
+} from '@material-ui/icons';
+
+import Tooltip from '../../utils/components/Tooltip';
+
+import { sidebarActions } from '../../reducers/ducks';
+
+const DarkModeToggle = ({ type, tooltipVisible, edge }) => {
+  const darkMode = useSelector(state => state.sidebar.darkMode);
+  const dispatch = useDispatch();
+
+  const handleToggleLights = () => {
+    dispatch(sidebarActions.toggleLights());
+  };
+
+  switch (type) {
+    case 'iconButton':
+      return (
+        <IconButton aria-label="menu" onClick={handleToggleLights} edge={edge}>
+          {darkMode ? <Brightness2TwoTone /> : <BrightnessLowTwoTone /> }
+        </IconButton>
+      );
+    case 'listItem':
+      return (
+        <ListItem button onClick={handleToggleLights}>
+          <Tooltip title="Toggle lights" placement="right" visible={tooltipVisible}>
+            <ListItemIcon>
+              {darkMode ? <Brightness2TwoTone /> : <BrightnessLowTwoTone /> }
+            </ListItemIcon>
+          </Tooltip>
+          <ListItemText primary="Toggle lights"/>
+        </ListItem>
+      );
+    default:
+      return <div>Type not supported.</div>
+  };
+};
+
+export default DarkModeToggle;
