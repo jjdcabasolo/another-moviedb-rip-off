8d48693cae5599c3e176c8d6d8fad9d85cd914c0
jjdcabasolo <abasolojohnjourish@gmail.com>
2021-03-20 20:28:00 +0800

    Updates

---
+++ precache-manifest.2729c5f5f95822df74bb4bf2a8818b36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298e3faf865f5f5ae8157616e483a03b",
+    "url": "/another-moviedb-rip-off/index.html"
+  },
+  {
+    "revision": "c6ea345c0e27cd4127b9",
+    "url": "/another-moviedb-rip-off/static/css/main.9a44514e.chunk.css"
+  },
+  {
+    "revision": "0e8f1c3bc84cb1f87dde",
+    "url": "/another-moviedb-rip-off/static/js/0.d1377c81.chunk.js"
+  },
+  {
+    "revision": "5e53869ded8aab1b35ec",
+    "url": "/another-moviedb-rip-off/static/js/1.ed642610.chunk.js"
+  },
+  {
+    "revision": "09450a00208f8ce68175",
+    "url": "/another-moviedb-rip-off/static/js/2.9d16aa22.chunk.js"
+  },
+  {
+    "revision": "b4b159d19b62824cc492",
+    "url": "/another-moviedb-rip-off/static/js/3.cbc2f8a5.chunk.js"
+  },
+  {
+    "revision": "185b595ae65282af35dcea75a5c1ee78",
+    "url": "/another-moviedb-rip-off/static/js/3.cbc2f8a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4939ca6741bdf7aebe",
+    "url": "/another-moviedb-rip-off/static/js/4.ecf6495b.chunk.js"
+  },
+  {
+    "revision": "f28166ef35e75688e42b",
+    "url": "/another-moviedb-rip-off/static/js/7.25eb021b.chunk.js"
+  },
+  {
+    "revision": "596b857f8abaf0834979163e4b0cd78a",
+    "url": "/another-moviedb-rip-off/static/js/7.25eb021b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ea345c0e27cd4127b9",
+    "url": "/another-moviedb-rip-off/static/js/main.59dc18f2.chunk.js"
+  },
+  {
+    "revision": "0cb4ab47018efaadb2ba",
+    "url": "/another-moviedb-rip-off/static/js/runtime-main.ca2910b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/another-moviedb-rip-off/precache-manifest.c459a8c3b2fa47760a4e077febb3cb78.js"
+  "/another-moviedb-rip-off/precache-manifest.2729c5f5f95822df74bb4bf2a8818b36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/another-moviedb-rip-off/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
